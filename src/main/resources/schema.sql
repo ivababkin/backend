f9633c7bfd1404c1323e388517ceac7d9c2e12fb
IgorOzherelev <i9163073959@gmail.com>
2019-11-03 18:35:54 +0200

    added separate init for frnt

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -33,7 +33,3 @@ CREATE TABLE IF NOT EXISTS users_tasks  (
     CONSTRAINT users_tasks_pk PRIMARY KEY (user_id, task_id)
 );
 
-
-/*INSERT INTO users VALUES (1, 'lalal', 'pass', 'email', 'name', 'surname', false);*/
-INSERT INTO tasks VALUES (1, 1, '1', '1', '/1', 1 ,'one');
-INSERT INTO users_tasks VALUES (1, 1, 0, 'lalal', 0, null, 'logarifm', 'code');

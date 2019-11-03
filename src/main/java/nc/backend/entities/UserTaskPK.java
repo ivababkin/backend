a8b688ab4a35ce0ffd874eb347ab137a89c47a9f
IgorOzherelev <i9163073959@gmail.com>
2019-11-03 23:57:40 +0200

    added separate init for frnt

---
+++ src/main/java/nc/backend/entities/UserTaskPK.java
@@ -0,0 +1,31 @@
+package nc.backend.entities;
+
+
+import com.sun.istack.NotNull;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+
+@EqualsAndHashCode
+@NoArgsConstructor
+public class UserTaskPK {
+    @Getter
+    @Setter
+    @Column(name = "USER_ID")
+    @NotNull
+    private Long userId;
+
+    @Getter
+    @Setter
+    @Column(name = "TASK_ID")
+    @NotNull
+    private Long taskId;
+
+    public UserTaskPK(Long userId, Long taskId) {
+        this.userId = userId;
+        this.taskId = taskId;
+    }
+}

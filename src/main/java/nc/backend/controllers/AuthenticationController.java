8ec30b0c007c6918958e9944233b2e9a4f8aadef
IgorOzherelev <i9163073959@gmail.com>
2019-11-08 02:06:20 +0300

    added auth controller

---
+++ src/main/java/nc/backend/controllers/AuthenticationController.java
@@ -0,0 +1,63 @@
+package nc.backend.controllers;
+
+import nc.backend.common.Role;
+import nc.backend.dtos.AuthenticationRequestDto;
+import nc.backend.entities.User;
+import nc.backend.security.jwt.JwtTokenProvider;
+import nc.backend.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserService userService;
+
+
+    public AuthenticationController(AuthenticationManager authenticationManager,
+                                    JwtTokenProvider jwtTokenProvider, UserService userService) {
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity login(@RequestBody AuthenticationRequestDto requestDto) {
+        try {
+            String username = requestDto.getUsername();
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, requestDto.getPassword()));
+            User user = userService.findByUserLogin(username);
+
+            if (user == null) {
+                throw new UsernameNotFoundException("User with username: " + username + " not found");
+            }
+
+            String token = jwtTokenProvider.createToken(username, (List<Role>) new Role(user.getAdmin()));
+
+            System.out.println(token);
+            Map<Object, Object> response = new HashMap<>();
+            response.put("username", username);
+            response.put("token", token);
+
+            return ResponseEntity.ok(response);
+        } catch (AuthenticationException e) {
+            throw new BadCredentialsException("Invalid username or password");
+        }
+    }
+}

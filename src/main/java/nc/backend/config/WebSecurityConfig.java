2aa60e88cfbae0541ed8f801e3ed7a033d7809a6
ivanbabkin <ivanbabkin99@yandex.com>
2019-11-07 19:51:14 +0300

    hashing added

--- src/main/java/nc/backend/config/WebSecurityConfig.java
+++ src/main/java/nc/backend/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package nc.backend.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -12,13 +13,29 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.authorizeRequests().antMatchers("/").permitAll();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // Enable jdbc authentication
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(passwordEncoder());
     }
 }
 
--- src/main/java/nc/backend/services/UserAuthorizationService.java
+++ src/main/java/nc/backend/services/UserAuthorizationService.java
@@ -5,14 +5,17 @@ import nc.backend.dtos.UserRegistrationDto;
 import nc.backend.entities.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class UserAuthorizationService {
     private UserDataService userDataService;
-    private BCryptPasswordEncoder encoder;
+    @Autowired
+    private PasswordEncoder bCryptPasswordEncoder;
     private UserDao userDao;
 
     private static Logger logger = LoggerFactory.getLogger(UserAuthorizationService.class);
@@ -20,20 +23,15 @@ public class UserAuthorizationService {
 
     public UserAuthorizationService(UserDao userDao){
         this.userDao = userDao;
-        encoder = new BCryptPasswordEncoder();
+        bCryptPasswordEncoder = new BCryptPasswordEncoder();
         userDataService = new UserDataService();
     }
 
-    /*
-    public UserDto getUser(Long userId){
-        User user = userDao.findByID(userId);
-        return UserDataService.buildUserDtoFromUser(user);
-    }
-*/
     public User registerUser(UserRegistrationDto userRegistrationObject) {
         User user = this.userDataService.buildUserFromUserRegistrationDto(userRegistrationObject);
+        String encodedPassword = bCryptPasswordEncoder.encode(userRegistrationObject.getPassword());
+        user.setPassword(encodedPassword);
         userDao.save(user);
-
         logger.info("----------User saved-----------", user);
 
         return user;

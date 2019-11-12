e18b391a128234d320d2b22514fb1ebd5d7eaac4
IgorOzherelev <i9163073959@gmail.com>
2019-11-12 15:20:13 +0300

    Refactoring: added UserAuthenticationService, logic method login moved from UserAuthenticationController to UserAuthenticationService

--- src/main/java/nc/backend/controllers/UserAuthenticationController.java
+++ src/main/java/nc/backend/controllers/UserAuthenticationController.java
@@ -1,57 +1,25 @@
 package nc.backend.controllers;
 
-import nc.backend.common.Role;
 import nc.backend.dtos.AuthenticationAnswerDto;
 import nc.backend.dtos.AuthenticationRequestDto;
-import nc.backend.entities.User;
-import nc.backend.security.jwt.JwtTokenProvider;
-import nc.backend.services.UserService;
+import nc.backend.services.UserAuthenticationService;
 import org.springframework.http.MediaType;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("/")
 public class UserAuthenticationController {
 
-    private final AuthenticationManager authenticationManager;
-    private final JwtTokenProvider jwtTokenProvider;
-    private final UserService userService;
+    private UserAuthenticationService userAuthenticationService;
 
-
-    public UserAuthenticationController(AuthenticationManager authenticationManager,
-                                        JwtTokenProvider jwtTokenProvider, UserService userService) {
-        this.authenticationManager = authenticationManager;
-        this.jwtTokenProvider = jwtTokenProvider;
-        this.userService = userService;
+    public UserAuthenticationController(UserAuthenticationService userAuthenticationService) {
+        this.userAuthenticationService = userAuthenticationService;
     }
 
     @RequestMapping(value = "/auth", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public AuthenticationAnswerDto login(@RequestBody AuthenticationRequestDto requestDto) {
-        try {
-            String username = requestDto.getUsername();
-            User user = userService.findByUserLogin(username);
-
-            if (user == null) {
-                throw new UsernameNotFoundException("User with username: " + username + " not found");
-            }
-
-            List<Role> roles = new ArrayList<>();
-            roles.add(new Role(user.getAdmin()));
-            String token = jwtTokenProvider.createToken(username, roles);
-
-
-
-            return new AuthenticationAnswerDto(username, token);
-        } catch (AuthenticationException e) {
-            throw new BadCredentialsException("Invalid username or password");
-        }
+        return this.userAuthenticationService.login(requestDto);
     }
 }

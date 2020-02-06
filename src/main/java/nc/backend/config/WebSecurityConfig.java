29d8a56668876ccb16f20a667a8182bff91cc029
IgorOzherelev <i9163073959@gmail.com>
2020-02-06 23:56:27 +0300

    final security

--- src/main/java/nc/backend/config/WebSecurityConfig.java
+++ src/main/java/nc/backend/config/WebSecurityConfig.java
@@ -26,9 +26,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private DataSource dataSource;
     private JwtTokenProvider jwtTokenProvider;
 
-    private  String ADMIN_ENDPOINT = "/admin";
-    private  String REGISTER_ENDPOINT = "/register";
-    private  String AUTH_ENDPOINT = "/**";
+    private final String REGISTER_ENDPOINT = "/register";
+    private final String AUTH_ENDPOINT = "/auth";
 
     @Autowired
     public WebSecurityConfig(DataSource dataSource, JwtTokenProvider jwtTokenProvider) {
@@ -59,7 +58,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers(AUTH_ENDPOINT).permitAll()
+                .antMatchers(AUTH_ENDPOINT, REGISTER_ENDPOINT).permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .apply(new JwtConfigurer(jwtTokenProvider));
